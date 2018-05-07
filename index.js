dbe7c1f3dba9f657e1f676e5cdd326f016ce6b5c
github username <jeremyagottfried@gmail.com>
2018-05-07 15:56:47 -0400

    basic front end fetches

---
+++ index.js
@@ -0,0 +1,28 @@
+let postData = {
+  "name":"Jeremy","password":"password"
+}
+
+fetch('http://localhost:3000/authenticate', {
+  method: 'POST',
+  body: JSON.stringify(postData),
+  headers: {
+    mode:'no-cors',
+    'Accept': 'application/json',
+    "Content-Type" : 'application/json'
+  }
+}).then((res) => res.json()).then((json) => {
+  let token = json.auth_token
+  get_user(token)
+})
+
+function get_user(token) {
+fetch('http://localhost:3000/api/v1/users/1', {
+  method: 'GET',
+  headers: {
+    Authorization: `token ${token}`,
+    mode: 'no-cors',
+    'Accept': 'application/json',
+    'content-type': 'application/json'
+  }
+}).then(res => (res.json())).then(console.log)
+}
