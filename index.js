53165eb1ccebc0d4127c1641ec62bf5a86e979d8
github username <jeremyagottfried@gmail.com>
2018-05-08 14:04:06 -0400

    added simple validate for username/password

--- index.js
+++ index.js
@@ -42,7 +42,14 @@ let renderMain = function() {
   });
 }
 
-let renderLogin = function() {
+let renderLogin = function(errors = []) {
+  errors.forEach(el => {
+    let error = document.createElement('h1')
+    error.className = 'error'
+    error.innerHTML = el
+    document.body.prepend(error)
+  }
+  )
   mainContentContainer.innerHTML = `
     <form id=loginForm>
       Username:<br>
@@ -54,6 +61,8 @@ let renderLogin = function() {
   loginForm.onsubmit = (e) => {
     e.preventDefault()
 
+    let errors = document.querySelectorAll('.error')
+    errors.forEach(el => el.remove())
     const postData = {}
     postData.name = userName.value
     postData.password = password.value
@@ -66,10 +75,17 @@ let renderLogin = function() {
         'Accept': 'application/json',
         "Content-Type" : 'application/json'
       }
-    }).then((res) => res.json()).then((json) => {
+    }).then((res) => {
+      if (res.status === 401) {
+    renderLogin(["Invalid Username or Password"])
+  } else {
+    return res.json()
+  }
+}).then((json) => {
+  if (typeof json === 'object') {
       console.log(json)
       let token = json.auth_token
-      get_user(token, postData.name)
+      get_user(token, postData.name) }
     })
 
   }
@@ -100,8 +116,7 @@ fetch('http://localhost:3000/api/v1/users/' + name, {
   var now = new Date();
 now.setTime(now.getTime() + 1 * 36000 * 1000);
 document.cookie = `user_id=${json.id}; expires=` + now.toUTCString() + "; path=/";
-
-
+document.cookie = `user_name=${json.name}; expires=` + now.toUTCString() + "; path=/";
 
 
 renderMain()
